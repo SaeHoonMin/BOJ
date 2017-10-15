13036a9847c1b0e80792d4d5d94910358b2c8af3
senymin <seny_min@naver.com>
2017-10-15 21:09:28 +0900

    숫자 카드

---
+++ BOJ/10816/10816.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <math.h>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <string.h>
+#include <map>
+using namespace::std;
+
+
+
+int main(void)
+{
+	/*
+	10816 : 숫자 카드
+
+	*/
+	map<int, int> mymap;
+	map<int, int>::iterator it;
+	int n, m;
+	cin >> n;
+	for (int i= 0; i < n; i++)
+	{
+		int num;
+		scanf("%d", &num);
+		it = mymap.find(num);
+		if (it != mymap.end())
+			(it->second)++;
+		else
+		{
+			mymap[num] = 1;
+		}
+
+	}
+	cin >> m;
+	for (int i = 0;i < m; i++)
+	{
+		int num; 
+		scanf("%d", &num);
+		it = mymap.find(num);
+		if (it != mymap.end())
+			printf("%d", it->second);
+		else
+			printf("0");
+
+		if (i < m - 1)
+			printf(" ");
+
+	}
+
+}
